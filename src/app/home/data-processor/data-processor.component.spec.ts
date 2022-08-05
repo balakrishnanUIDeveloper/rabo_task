59b0cb9cc7bcabe49cf148f26961a752ecab1076
LAPTOP-RQB3NDNV\BALAKRISHNAN <krish.18.2009@gmail.com>
2022-08-05 16:13:50 +0200

    test: test case

--- src/app/home/data-processor/data-processor.component.spec.ts
+++ src/app/home/data-processor/data-processor.component.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HomeService } from '../home.service';
+import { PROCESSOR_TITLE } from '../home.constants';
 import { DataProcessorComponent } from './data-processor.component';
 
 describe('DataProcessorComponent', () => {
   let component: DataProcessorComponent;
   let fixture: ComponentFixture<DataProcessorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DataProcessorComponent]
-    }).compileComponents();
-
+  beforeEach(() => {
+    const homeServiceStub = () => ({ recordSubject: { next: () => ({}) } });
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [DataProcessorComponent],
+      providers: [{ provide: HomeService, useFactory: homeServiceStub }]
+    });
     fixture = TestBed.createComponent(DataProcessorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`records has default value`, () => {
+    expect(component.records).toEqual([]);
+  });
+
+  it(`title has default value`, () => {
+    expect(component.title).toEqual(PROCESSOR_TITLE);
   });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ModalService } from '../shared/modal/modal.service';
+import { HomeService } from './home.service';
+import { APPTITLE } from './home.constants';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -7,9 +10,22 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(() => {
+    const modalServiceStub = () => ({
+      modalPopupSubject: { next: () => ({}) }
+    });
+    const homeServiceStub = () => ({
+      recordSubject: { subscribe: (f: any) => f({status: ''}), unsubscribe: () => ({}) },
+      isValidCSVFile: () => ({}),
+      isValidXMLFile: () => ({}),
+      checkRecordsDataFromFile: () => ({})
+    });
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [
+        { provide: ModalService, useFactory: modalServiceStub },
+        { provide: HomeService, useFactory: homeServiceStub }
+      ]
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -17,5 +33,27 @@ describe('HomeComponent', () => {
 
   it('can load instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`records has default value`, () => {
+    expect(component.records).toEqual([]);
+  });
+
+  it(`title has default value`, () => {
+    expect(component.title).toEqual(APPTITLE);
+  });
+
+  it(`duplicateReferenceIDs has default value`, () => {
+    expect(component.duplicateReferenceIDs).toEqual([]);
+  });
+
+  describe('ngOnInit', () => {
+    it('makes expected calls', () => {
+      spyOn(component, 'fileReset').and.callThrough();
+      spyOn(component, 'getDuplicateRecord').and.callThrough();
+      component.ngOnInit();
+      expect(component.fileReset).toHaveBeenCalled();
+      expect(component.getDuplicateRecord).toHaveBeenCalled();
+    });
   });
 });
