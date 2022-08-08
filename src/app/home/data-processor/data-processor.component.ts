5b67f1b72c3020ee10f2eae1a3e5cbefe5d4645f
balakrishnanUIDeveloper <krish.18.2009@gmail.com>
2022-08-08 21:39:43 +0200

    fix: version change (#1)

--- src/app/home/data-processor/data-processor.component.ts
+++ src/app/home/data-processor/data-processor.component.ts
@@ -13,17 +13,17 @@ import { HomeService } from '../home.service';
   templateUrl: './data-processor.component.html'
 })
 export class DataProcessorComponent {
+  resetBtnLabel = RESET_BTN;
+  title = PROCESSOR_TITLE;
   @Input() records: AccountSData[] = [];
   @Output() uploadFileEvent = new EventEmitter<string>();
   @ViewChild('csvReader') csvReader: any;
-  resetBtnLabel = RESET_BTN;
-  title = PROCESSOR_TITLE;
-  constructor(private homeService: HomeService) {}
+  constructor(private _homeService: HomeService) {}
   uploadListener($event: any): void {
     this.uploadFileEvent.emit($event);
   }
   fileReset() {
     this.csvReader.nativeElement.value = '';
-    this.homeService.recordSubject.next({ status: RECORDS_EVENT.RESET });
+    this._homeService.recordSubject.next({ status: RECORDS_EVENT.RESET });
   }
 }
